ac25dbdb0318b78d7457eed86b588a8333615177
Terry M. Poulin <BigBoss1964@gmail.com>
2026-03-15 00:41:35 +0000

    More test coverage of BackupSpecs.

--- BackupSpec_test.go
+++ BackupSpec_test.go
@@ -5,59 +5,88 @@ package main
 
 import (
 	"encoding/json"
+	"os"
 	"slices"
 	"testing"
 
 	"gopkg.in/yaml.v3"
 )
 
-func TestUnmarshalBackupSpecs(t *testing.T) {
-	expected := []BackupSpec{
-		{
-			Name:     "firstspec",
-			Path:     "firstpath",
-			Format:   "firstformat",
-			Contents: []string{"foo", "bar"},
-		},
-		{
-			Name:     "secondspec",
-			Path:     "secondpath",
-			Format:   "secondformat",
-			Contents: []string{"ham", "spam", "eggs"},
-		},
-	}
+var testBackupSpecs = []BackupSpec{
+	{
+		Name:     "firstspec",
+		Path:     "firstpath",
+		Format:   "firstformat",
+		Contents: []string{"foo", "bar"},
+	},
+	{
+		Name:     "secondspec",
+		Path:     "secondpath",
+		Format:   "secondformat",
+		Contents: []string{"ham", "spam", "eggs"},
+	},
+}
+
+func assertBackupSpecs(t *testing.T, actual, expected []BackupSpec) bool {
 	eq := func(e1, e2 BackupSpec) bool {
 		return e1.Name == e2.Name &&
 			e1.Path == e2.Path &&
 			e1.Format == e2.Format &&
 			slices.Equal(e1.Contents, e2.Contents)
 	}
-	t.Run("JSON", func(t *testing.T) {
+	if !slices.EqualFunc(actual, expected, eq) {
+		t.Errorf("specs do not match:\nactual  : %+v\nexpected: %+v\n", actual, expected)
+		return true
+	}
+	return false
+}
+
+func TestUnmarshalBackupSpecs(t *testing.T) {
+	assert := func(t *testing.T, expected []BackupSpec, marshaler func(any) ([]byte, error)) {
 		data, err := json.MarshalIndent(expected, "", "    ")
 		if err != nil {
-			t.Fatalf("json.Marshal failed: %v", err)
+			t.Fatalf("marshaling failed: %v", err)
 		}
 		actual, err := UnmarshalBackupSpecs(data)
 		if err != nil {
-			t.Fatalf("UnmarshalBackupSpecs() from JSON failed: %v", err)
+			t.Fatalf("UnmarshalBackupSpecs() failed: %v", err)
 		}
-		t.Logf("actual: %v", actual)
-		if !slices.EqualFunc(actual, expected, eq) {
-			t.Errorf("specs do not match:\nactual  : %+v\nexpected: %+v\n", actual, expected)
-		}
+		assertBackupSpecs(t, actual, expected)
+	}
+	t.Run("JSON", func(t *testing.T) {
+		assert(t, testBackupSpecs, json.Marshal)
 	})
 	t.Run("YAML", func(t *testing.T) {
-		data, err := yaml.Marshal(expected)
+		assert(t, testBackupSpecs, yaml.Marshal)
+	})
+}
+
+func TestBackupSpecsFromFile(t *testing.T) {
+	assert := func(t *testing.T, expected []BackupSpec, marshaler func(any) ([]byte, error)) {
+		fp, err := os.CreateTemp("", "")
 		if err != nil {
-			t.Fatalf("yaml.Marshal failed: %v", err)
+			t.Fatalf("os.CreateTemp() failed: %v", err)
 		}
-		actual, err := UnmarshalBackupSpecs(data)
+		name := fp.Name()
+		defer os.Remove(name)
+		data, err := marshaler(expected)
 		if err != nil {
-			t.Fatalf("UnmarshalBackupSpecs() from YAML failed: %v", err)
+			t.Fatalf("marshaling failed: %v", err)
 		}
-		t.Logf("actual: %v", actual)
-		if !slices.EqualFunc(actual, expected, eq) {
-			t.Errorf("specs do not match:\nactual  : %+v\nexpected: %+v\n", actual, expected)
+		if _, err = fp.Write(data); err != nil {
+			t.Fatalf("Writing marshaled data failed: %v", err)
 		}
+		fp.Close()
+		actual, err := BackupSpecsFromFile(name)
+		if err != nil {
+			t.Errorf("BackupSpecsFromFile(%q) failed: %v", name, err)
+		}
+		assertBackupSpecs(t, actual, expected)
+	}
+	t.Run("JSON", func(t *testing.T) {
+		assert(t, testBackupSpecs, json.Marshal)
+	})
+	t.Run("YAML", func(t *testing.T) {
+		assert(t, testBackupSpecs, yaml.Marshal)
 	})
 }
